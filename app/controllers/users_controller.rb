0740a1592a54dee825dc394c3a9862d06256a1b6
Tocyuki <tocguitar1@gmail.com>
2016-06-06 18:17:22 +0900

    modified users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,16 @@ class UsersController < ApplicationController
 
   def show
     @user = Hash.new
-    @user[:name] = 'Shohei Aoki'
-    @user[:username] = 'moyahima'
-    @user[:location] = 'Tottori, Japan'
-    @user[:about] = 'Nice to meet you.'
+    if params[:username] == 'ryooopan'
+      @user[:name] = 'Ryo Suzuki'
+      @user[:username] = 'ryoopan'
+      @user[:location] = 'Kanagawa, Japan'
+      @user[:about] = 'Hello, I an Ryo.'
+    elsif params[:username] == 'moyahima'
+      @user[:name] = 'Shohei Aok'
+      @user[:username] = 'moyahima'
+      @user[:location] = 'Tottori, Japan'
+      @user[:about] = 'Nice to meet you.'
+    end
   end
 end
